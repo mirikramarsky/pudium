f8ac7197254ed9d4dcb086dc46369f31a6993dad
mirikramarsky <talmida.tichnut.miriam.karmarsky@byb.org.il>
2025-06-17 18:33:45 +0300

    add

--- server/BL/schoolService.js
+++ server/BL/schoolService.js
@@ -4,6 +4,14 @@ class SchoolService extends BaseService{
     constructor(){
         super(schoolRepository);
     }
+     async insert(params){
+        if (!params.fields)
+            params.fields = JSON.stringify(["שירה", "מחול", "אימון שיר", "תפאורה","כתיבת שיר", "עריכה", "הצגה", "אימון הצגה"])
+        let result = await this.repository.insert(params);
+        if(result)
+            return result
+        throw new idError("this id is exist")
+    }
 }
 let schoolService = new SchoolService();
 module.exports = schoolService;
--- server/DAL/Repositories/schoolRepository.js
+++ server/DAL/Repositories/schoolRepository.js
@@ -16,11 +16,24 @@ class SchoolRepository {
         ); 
         return school;
     }
-    async update(SchoolCode, params) {
-        let school = await pool.query(` UPDATE schools
-            SET schoolname = $1
-            WHERE id = $2 `,[params.SchoolName, SchoolCode]);
-        return school.rowCount;
+    async update(id, updatedFields) {
+        const sets = [];
+        const values = [];
+        let i = 1;
+
+        for (const key in updatedFields) {
+            if (updatedFields[key] !== undefined) {
+                sets.push(`${key} = $${i++}`);
+                values.push(updatedFields[key]);
+            }
+        }
+
+        if (sets.length === 0) return { message: 'Nothing to update.' };
+
+        values.push(id);
+        const query = `UPDATE schools SET ${sets.join(', ')} WHERE id = $${i}`;
+        const result = await pool.query(query, values);
+        return result.rowCount > 0;
     }
     async delete(SchoolCode) {
         let school = await pool.query(` DELETE FROM schools
