69092a0453b89e3169f46c17cf9d108ffb248dc5
mirikramarsky <talmida.tichnut.miriam.karmarsky@byb.org.il>
2025-07-29 15:06:24 +0300

    add changes

--- server/routes/stuInSeaRoute.js
+++ server/routes/stuInSeaRoute.js
@@ -44,9 +44,9 @@ router.get('/student/:id', async(req, res,next)=>{
     }
 });
 
-router.get('/search/:id', async(req, res,next)=>{
+router.get('/search/:id/:schoolId', async(req, res,next)=>{
     try{
-        let result = await stuInSeaService.getBySearchId(req.params.id)
+        let result = await stuInSeaService.getBySearchId(req.params.id, req.params.schoolId);
         console.log("results", result);
         
         if(result != undefined)
